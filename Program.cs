613f8a4ef6e9efed71bf50a7e96d3cb6ed43e353
3sph <141910502+3sph@users.noreply.github.com>
2023-11-24 15:33:20 +0100

    fikset

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace variabel_test2._0
+{
+    internal class Program
+    {
+        //private static object Console;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Espen";
+            Console.WriteLine($"mitt navn er {name}");
+            int age = 30;
+            char forbokstav = 'E';
+            Console.WriteLine($"Første bokstaven i mitt navn er {forbokstav}");
+            Console.WriteLine($"jeg er {age} år gammel");
+            double høyde = 1.81;
+            Console.WriteLine($"jeg er {høyde} cm");
+            float pi = 3.14F;
+            Console.WriteLine("pi =" + pi);
+            bool truePi = true;
+            Console.WriteLine(truePi);
+            decimal pi2 = 3.14159M;
+            Console.WriteLine($" ett høyere tall for pi er {pi2}");
+            long timeWasted = 946707779;
+            Console.WriteLine($"mitt liv i sekunder er {timeWasted} sekunder");
+
+
+            int a = 5;
+            decimal b = 3.0M;
+            decimal result = a + b;
+            Console.WriteLine($"resultatet av stykket i oppgave 2 blir: {result}");
+        }
+      
+                    }
+
+
+        
+    }
